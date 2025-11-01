918855ebbc17adfbc8d8ce1ddb9acae4f7695afe
GustavoSantSan <111728328+GustavoSantSan@users.noreply.github.com>
2025-10-31 22:31:43 -0600

    Se agrega camara de piso

---
+++ Camera.cpp
@@ -0,0 +1,102 @@
+#include "Camera.h"
+
+Camera::Camera() {}
+
+Camera::Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed)
+{
+	position = startPosition;
+	worldUp = startUp;
+	yaw = startYaw;
+	pitch = startPitch;
+	front = glm::vec3(0.0f, 0.0f, -1.0f);
+
+	moveSpeed = startMoveSpeed;
+	turnSpeed = startTurnSpeed;
+
+	update();
+}
+
+void Camera::keyControl(bool* keys, GLfloat deltaTime)
+{
+	GLfloat velocity = moveSpeed * deltaTime;
+
+  // proyectar el vector front sobre el plano XZ (quitarle el componente Y)
+  glm::vec3 flatFront = glm::normalize(glm::vec3(front.x, 0.0f, front.z));
+
+	if (keys[GLFW_KEY_W])
+	{
+		position += front * velocity;
+	}
+
+	if (keys[GLFW_KEY_S])
+	{
+		position -= front * velocity;
+	}
+
+	if (keys[GLFW_KEY_A])
+	{
+		position -= right * velocity;
+	}
+
+	if (keys[GLFW_KEY_D])
+	{
+		position += right * velocity;
+	}
+
+
+  // mantener altura constante (por ejemplo, Y=1.8f)
+  position.y = 4.0f;
+}
+
+void Camera::mouseControl(GLfloat xChange, GLfloat yChange)
+{
+	xChange *= turnSpeed;
+	yChange *= turnSpeed;
+
+	yaw += xChange;
+	pitch += yChange;
+
+	if (pitch > 89.0f)
+	{
+		pitch = 89.0f;
+	}
+
+	if (pitch < -89.0f)
+	{
+		pitch = -89.0f;
+	}
+
+	update();
+}
+
+glm::mat4 Camera::calculateViewMatrix()
+{
+	return glm::lookAt(position, position + front, up);
+}
+
+glm::vec3 Camera::getCameraPosition()
+{
+	return position;
+}
+
+
+glm::vec3 Camera::getCameraDirection()
+{
+	return glm::normalize(front);
+}
+
+void Camera::update()
+{
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front = glm::normalize(front);
+
+	right = glm::normalize(glm::cross(front, worldUp));
+	up = glm::normalize(glm::cross(right, front));
+}
+
+
+Camera::~Camera()
+{
+}
